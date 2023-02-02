6f5aae45c9666312892d266b43344f6502f70c68
Hadi-git7 <abouhomeinh@gmail.com>
2023-02-02 16:35:20 +0200

    Completed navbar/slideshow/about sections with fetching from backend

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,8 +1,41 @@
-import React from 'react'
-import '../About/About.css'
+import React, {useState,useEffect} from 'react';
+import '../About/About.css';
+import Axios from 'axios';
+
+
 function About() {
+  const [about, setAbout] = useState([])
+  const Request = async () => {
+    try {
+      const response = await  Axios.get('http://localhost:5000/api/about')
+      const res = await response.data;
+      console.log(res)
+      setAbout(res)
+    }catch(err){
+      console.log(err)
+    }
+  }
+  useEffect(() =>{
+    Request()
+  },[]);
+
+
   return (
     <>
+    <div className='About-main' id='about'>
+      <div className='About-content'>
+          <div className='About-image'><img src={`http://localhost:5000/${about[0]?.image}`} /></div>
+
+          <div className='About-text'>
+          <h1 className='About-h1'>About</h1>
+          <img className='hide-show' src={`http://localhost:5000/${about[0]?.image}`} />
+          <div className='wrep'>
+            <h3 className='About-h3'>{about[0]?.title}</h3>
+            <p className='About-p'>{about[0]?.text}</p>
+          </div>
+          </div>
+      </div>
+    </div>
     </>
   )
 }
--- src/Components/Slideshow/Slideshow.js
+++ src/Components/Slideshow/Slideshow.js
@@ -1,8 +1,62 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import '../Slideshow/Slideshow.css'
+import Axios from 'axios';
+
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+
 function Slideshow() {
+  const [announcements, setAnnouncements] = useState([])
+  const Request = async () => {
+    try {
+      const response = await  Axios.get('http://localhost:5000/api/announcements')
+      const res = await response.data;
+      console.log(res)
+      setAnnouncements(res)
+    }catch(err){
+      console.log(err)
+    }
+  }
+  useEffect(() =>{
+    Request()
+  },[]);
   return (
    <>
+   <div className='main-slideshow'>
+   <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {announcements?.map((obj) => {
+          return <SwiperSlide key={obj._id}>
+            <div className='container'>
+            <img src={`http://localhost:5000/${obj.image}`} alt={obj.title} />
+            </div>
+          </SwiperSlide>
+        })}
+      </Swiper>
+      </div>
    </>
   )
 }
