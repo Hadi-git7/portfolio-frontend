f0d18b097b6590e1cfc34bd42478af24498330b8
Mohammad-Harkous <mohammad.a.harkous@gmail.com>
2023-02-06 17:01:13 +0200

    Project section completed

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,26 +1,31 @@
-import React from 'react'
-import '../Projects/Projects.css'
-//import axios from 'axios'
-import { useState, useEffect } from 'react'
-//import Rectangle from './images/Rectangle.png'
+import React from 'react';
+import '../Projects/Projects.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+
 
 
 function Projects() {
+  
+  const [projects, setProjects] = useState([]);
+  const [visible, setVisible] = useState(3);
 
-  const [projects, setProjects] = useState(null)
-
+  const showMoreItems = () => {
+    setVisible((prevValue) => prevValue + 3);
+  }
 
 useEffect(() => {
   const fetchProjects = async () => {
-    const response = await fetch('/api/project')
-    const json = await response.json()
-
-    if(response.ok){
-      setProjects(json)
+    try{
+      const response = await axios.get('/api/project');
+      const result = await response.data;
+      setProjects(result)
+    }catch(err){
+      console.log(err);
     }
   }
-  
-  fetchProjects()
+  fetchProjects();
 },[])
 
 
@@ -35,21 +40,30 @@ useEffect(() => {
 
         <div className='project-images'>
 
-        {projects && projects.map((project) => (
+          {projects && projects.slice(0, visible).map((project) => (
+          
           <div className='project-images-img' key={project._id}> 
            
-           <img  src= {project.image} alt={project.title}/> 
            
-           <p>{ project.description}</p>
-           
-          
-          </div> 
+            <img src= {project.image} alt={project.title} className='image'/>
+            <div className='overlay'>
+              {/**put in the decription section the link of website that you want to show  */}
+            <a href={ project.description} target='_blank' rel='noreferrer' className='text'>{ project.description}</a>
+            <span> {project.title}</span>
+           <hr className='hr'/>
+           </div>
+           </div>
           ))
            }
-        </div>
 
+        </div>
+        
+        
+        <button onClick={showMoreItems}>Show More</button> 
+       
       </div>
        
+
       
     </>
   )
