848e151735752618a8e43a330d59ca5e45566a7b
Rifaat-SaroutB09 <rifaat.sarout2@gmail.com>
2023-02-13 09:28:48 +0200

    added loginpage and protected route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-
+import { useEffect, useState } from "react";
+import Login from "./Dashboard/Login/Login";
 import Dashboard from "./Dashboard/dashboard/dashboard";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -7,14 +8,27 @@ import User from "./User/user";
 
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    window.localStorage.getItem("loggedIn")
+  );
+
+  useEffect(() => {
+    setIsLoggedIn(window.localStorage.getItem("loggedIn"));
+  }, []);
+
   return (
     <div>
-  <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<User />} />
-      <Route path="/dash" element={<Dashboard />} />
-    </Routes>
-  </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<User />} />
+          <Route path="/admin" element={<Login />} />
+          <Route
+            exact
+            path="/dash"
+            element={isLoggedIn === "true" ? <Dashboard /> : <Login />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
